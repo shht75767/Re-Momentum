b633a3f45e7d913739dc56bcaa638f2888cb3e43
HyeonTae-No <88132674+shht75767@users.noreply.github.com>
2021-10-01 23:23:46 +0900

    momentum-start

---
+++ js/clock.js
@@ -0,0 +1,14 @@
+const clock = document.querySelector(".clock-main")
+console.log(clock);
+
+
+
+function getTime(){
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2,"0");
+    const minutes = String(date.getMinutes()).padStart(2,"0");   
+    const seconds = String(date.getSeconds()).padStart(2,"0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`    
+};
+getTime();
+setInterval(getTime,1000)
