d09e014e0ca5cd1a5af5da7f767d962c69e7092d
HyeonTae-No <88132674+shht75767@users.noreply.github.com>
2021-10-03 18:45:50 +0900

    add todo

--- js/todo.js
+++ js/todo.js
@@ -7,22 +7,50 @@ let toDos = []
 
 function saveTodo() {
     localStorage.setItem("todo_list", JSON.stringify(toDos))
-
+}
+function saveClearTodo(todoObj){
+    localStorage.setItem("clear_todo", JSON.stringify(clearTodos))
 }
 
 function deleteTodo(event){
     const li = event.target.parentElement;
     li.remove()
-    toDos = toDos.filter((todo) => todo.id != li.id)
+    console.log(li);
+    toDos = toDos.filter((todo) =>todo.id != li.id)
     saveTodo()
 }
+
 let clearTodos = []
 
-function clearTodo(event){
-    
-    
-}
+const clearTodoList = localStorage.getItem("clear_todo")
 
+
+function clearTodo(event){
+    if(clearTodoList){
+        const parseClearTodo = JSON.parse(clearTodoList)
+        clearTodos
+    }
+    clearTodos.push(toDos)
+    const li = event.target.parentElement;
+        clearTodos = clearTodos.filter((todo) => todo.id = li.id)
+        clearTodos.forEach(paintClearTodo)
+        saveClearTodo()
+    }
+
+function paintClearTodo(todoObj){
+    const li = document.createElement("li")
+    const span = document.createElement("span")
+    const btn1 = document.createElement("button")
+    const btn2 = document.createElement("button")
+    span.innerText = todoObj.text
+    btn1.innerText = "❌"
+    btn2.innerText = "👍"
+    li.id = todoObj.id
+    li.appendChild(span)
+    li.appendChild(btn1)
+    clearUl.appendChild(li)
+    btn1.addEventListener("click", deleteTodo)
+}
 function paintTodo(todoObj){
     const li = document.createElement("li")
     const span = document.createElement("span")
@@ -46,18 +74,17 @@ function todoSubmit(event) {
     todoInput.value = "";
     const todoObj = {
         text : todo,
-        id : Math.random()
+        id : Date.now()
     }
-    clearTodos.push(todoObj)
     toDos.push(todoObj)
     paintTodo(todoObj)
     saveTodo()
+    saveClearTodo(todoObj)
 }
-
+ 
 todoForm.addEventListener("submit", todoSubmit)
 
 const saveTodoList = localStorage.getItem("todo_list")
-
 if(saveTodoList) {
     const parseTodo = JSON.parse(saveTodoList)
     toDos = parseTodo
